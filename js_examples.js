176994bb3cf2acf0ad0e1a8fac432b3be8d8439c
dani <dani.zaghian@gmail.com>
2019-02-11 11:24:53 -0800

    adds js vs ruby differences

---
+++ js_examples.js
@@ -0,0 +1,51 @@
+// var x = 10;
+
+// if (x == "10") {
+// 	console.log("equal");
+// } else {
+// 	console.log("not equal");
+// }
+
+// var x = 3;
+// if (null) {
+// 	x = x + 1;
+// }
+// if (false) {
+// 	x = x + 1;
+// }
+// if (0) {
+// 	x = x + 1;
+// }
+// if (!!"") {
+// 	x = x + 1;
+// }
+// console.log(x);
+
+// function printLyrics() {
+// 	console.log("Now this is a story all about how");
+// 	console.log("My life got flipped turned upside down");
+// 	console.log("And I'd like to take a minute, just sit right there");
+// 	console.log("I'll tell you how I became the prince of a town called Bel-Air");
+// }
+
+// printLyrics();
+
+// var x = 100;
+// function addOne(num) {
+// 	x = 1;
+// 	return num + x;
+// }
+// console.log(x); //100
+// console.log(addOne(5)); //6
+// console.log(x); //1
+
+var boat = {
+	name: "S. S. Minnow",
+	color: "white",
+	price: 20000
+}
+
+console.log(boat);
+console.log(boat.name);
+boat.color = "red";
+console.log(boat.color);
