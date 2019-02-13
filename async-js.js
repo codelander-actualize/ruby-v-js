2723406753f32d2bf4f8919358bdfb04bbacf104
dani <dani.zaghian@gmail.com>
2019-02-13 11:22:25 -0800

    adds async nested callbacks

--- async-js.js
+++ async-js.js
@@ -1,5 +1,9 @@
 setTimeout(function() {
 	console.log("first task done!");
+	setTimeout(function() {
+		console.log("second task done!");
+		setTimeout(function() {
+			console.log("third task done!");
+		}, 2000);
+	}, 2000);
 }, 2000);
-
-console.log("second task done!");
--- async-ruby.rb
+++ async-ruby.rb
@@ -1,3 +1,8 @@
 sleep 2
 puts "first task done!"
+
+sleep 2
 puts "second task done!"
+
+sleep 2
+puts "third task done!"
